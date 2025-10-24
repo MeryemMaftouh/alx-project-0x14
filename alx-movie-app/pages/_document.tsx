f9b1cf9da1c9ed4f06300c056f4952a46e4b9984
MeryemMaftouh <meryemmaftouh744@gmail.com>
2025-10-24 11:56:24 +0100

    Add Richer PWA Install UI: updated manifest and screenshots

--- alx-movie-app/pages/_document.tsx
+++ alx-movie-app/pages/_document.tsx
@@ -3,7 +3,13 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        {/* Link the manifest.json */}
+        <link rel="manifest" href="/manifest.json" />
+
+        {/* Theme color for browser UI */}
+        <meta name="theme-color" content="#0070f3" />
+      </Head>
       <body className="antialiased">
         <Main />
         <NextScript />
